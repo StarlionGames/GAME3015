d8216efa6093a0da1a2822acffc39ad1f331df5b
StarlionGames <starliongames.dev@gmail.com>
2026-02-16 19:33:06 -0500

    adding player and enemy

--- A1/Solution/InitializeDirect3D/Player.cpp
+++ A1/Solution/InitializeDirect3D/Player.cpp
@@ -1,1 +1,15 @@
 #include "Player.h"
+#include "Game.h"
+
+Player::Player(Game* game) : Entity(game) {
+	sprite = "player"; // change this with the name of the texture lol
+}
+
+void Player::DrawCurrent() const
+{
+}
+
+void Player::BuildCurrent()
+{
+	
+}
--- A1/Solution/InitializeDirect3D/Player.h
+++ A1/Solution/InitializeDirect3D/Player.h
@@ -1,5 +1,18 @@
 #pragma once
-class Player
+#include "Entity.hpp"
+#include <string>
+
+class Player : public Entity
 {
+public:
+	Player(Game* game);
+
+private:
+	virtual void DrawCurrent() const;
+	virtual void BuildCurrent();
+	
+	// --- variables ---
+
+	std::string sprite;
 };
 
